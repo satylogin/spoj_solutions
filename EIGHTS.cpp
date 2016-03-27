24f3cbb902947ee401dba0ca079616ee49175534
Satyarth Agrahari <satyarthagrahari@gmail.com>
2016-03-27 11:59:10 +0530

    Create EIGHTS.cpp

---
+++ EIGHTS.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	unsigned long long int i;
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		cin>>i;
+		cout<<192+((250)*(i-1))<<endl;
+	}
+	return 0;
+}
